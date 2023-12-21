437eb063cd227dc503b6df481a75d866a008b5f1
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-12-21 12:44:30 +0100

    9

---
+++ esercizio9.js
@@ -0,0 +1,23 @@
+SELECT *
+FROM tested
+WHERE survived = 1 AND sex = 'female' AND age > 30;
+
+//
+SELECT item
+FROM tested
+WHERE survived = 1 AND age = (SELECT MAX(age) FROM tested WHERE survived = 1);
+
+//
+SELECT *
+FROM tested
+WHERE embarked = 'C' AND fare < 50 AND fare > 20;
+
+//
+SELECT COUNT(*) AS "Number of survivors"
+FROM tested
+WHERE survived = 1;
+
+//
+SELECT *
+FROM tested
+WHERE embarked = 'C' AND fare > 75;
